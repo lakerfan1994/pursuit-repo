0a4b1ae8a0c4579b7d764d120fa7cd67c377a689
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2020-08-04 10:05:34 -0400

    lmaoooooed

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log(1 + 9);
+
+let thisAwesomeVariable = 9 * 9;
+
+
+console.log(thisAwesomeVariable);
+
+
+const neverChange = 'I will never change, bc im a never changing variable';
+
+console.log(neverChange + ' ' +  neverChange);
+  
+
+ let a = 9
+
+ a += 9;
+
+ console.log(a);
+
+ let product = 4 * 5;
+ product *= 5;
+
+ console.log(product);
